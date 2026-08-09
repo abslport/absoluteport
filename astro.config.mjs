df6dc155c766a3aacb9b71589562397a92ca4922
abslport <akunam622@gmail.com>
2026-08-10 02:20:25 +0700

    Create astro.config.mjs

---
+++ astro.config.mjs
@@ -0,0 +1,7 @@
+import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
+
+export default defineConfig({
+  integrations: [tailwind()],
+  site: 'https://bayz-dik.github.io/portfolio-bayu',
+});
