fd6b3e437490a181d4846282de5e195dff715ae1
abslport <akunam622@gmail.com>
2026-08-10 10:37:52 +0700

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,11 @@ import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
   integrations: [tailwind()],
-  site: 'https://bayz-dik.github.io/portfolio-bayu',
+  output: 'static',
+  site: 'https://bayu-portfolio.vercel.app',
+  vite: {
+    ssr: {
+      noExternal: ['three', 'gsap']
+    }
+  }
 });
